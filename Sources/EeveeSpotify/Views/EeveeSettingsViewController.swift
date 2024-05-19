9393f1e82ee88fdefc891d7b35896ba8ca12c65d
eevee <eeveerlm@gmail.com>
2024-05-19 15:29:53 +0300

    fix settings crash

--- Sources/EeveeSpotify/Views/EeveeSettingsViewController.swift
+++ Sources/EeveeSpotify/Views/EeveeSettingsViewController.swift
@@ -2,6 +2,24 @@ import SwiftUI
 import UIKit 
 
 class EeveeSettingsViewController: UIViewController {
+
+    override func conforms(to aProtocol: Protocol) -> Bool {
+        
+        if NSStringFromProtocol(aProtocol) ~= "SPTPageController" {
+            return true
+        }
+        
+        return super.conforms(to: aProtocol)
+    }
+
+    @objc func spt_pageIdentifier() -> String? {
+        return "EeveeSpotify"
+    }
+
+    @objc func spt_pageURI() -> NSURL? {
+        return NSURL(string: "")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
