e67f658184ca0f4e03ce983cff0a246b0fa409b5
Elliot <elliotchen2010@gmail.com>
2024-10-07 11:30:07 +0800

    Update EeveeAboutSettingsView.swift

--- Sources/EeveeSpotify/Settings/Views/Sections/EeveeAboutSettingsView.swift
+++ Sources/EeveeSpotify/Settings/Views/Sections/EeveeAboutSettingsView.swift
@@ -3,16 +3,13 @@ import SwiftUI
 struct EeveeAboutSettingsView: View {
     
     var body: some View {
-		List {
-			Section {
-					VStack(alignment: .leading) {
-						VStack(alignment: .leading) {
-							header: Text("about_main_title".localized),
-							Text("about_main_des".localized)
-						}
-					}
-				}
-			}
+        List {
+            Section {
+                VStack(alignment: .leading) {
+                    Text("about_main_title".localized)
+                    Text("about_main_des".localized)
+                }
+            }
         }
     }
 }
