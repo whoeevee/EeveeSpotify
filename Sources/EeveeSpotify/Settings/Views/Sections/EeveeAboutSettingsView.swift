c386d6c55868821d30cd0016520c9645bfeb9287
Elliot <elliotchen2010@gmail.com>
2024-10-08 20:00:23 +0800

    Update EeveeAboutSettingsView.swift

--- Sources/EeveeSpotify/Settings/Views/Sections/EeveeAboutSettingsView.swift
+++ Sources/EeveeSpotify/Settings/Views/Sections/EeveeAboutSettingsView.swift
@@ -1,134 +1,56 @@
 import SwiftUI
 
 struct EeveeAboutSettingsView: View {
+    let firstSectionLinks = [
+        ("whoeevee", "https://github.com/whoeevee"),
+        ("asdfzxcvbn", "https://github.com/asdfzxcvbn")
+    ]
+    
+    let secondSectionLinks = [
+        ("ElliotCHEN37", "https://github.com/ElliotCHEN37"),
+        ("Richard-NDC", "https://github.com/Richard-NDC"),
+        ("5jd", "https://github.com/5jd"),
+        ("UnexcitingDean", "https://github.com/UnexcitingDean"),
+        ("gototheskinny", "https://github.com/gototheskinny"),
+        ("LIKVIDATOR1337", "https://github.com/LIKVIDATOR1337"),
+        ("xiangfeidexiaohuo", "https://github.com/xiangfeidexiaohuo"),
+        ("wlxxd", "https://github.com/wlxxd"),
+        ("longopy", "https://github.com/longopy"),
+        ("yodaluca23", "https://github.com/yodaluca23"),
+        ("3xynos7", "https://github.com/3xynos7"),
+        ("An0n-00", "https://github.com/An0n-00"),
+        ("by3lish", "https://github.com/by3lish"),
+        ("emal0n", "https://github.com/emal0n"),
+        ("CukierDev", "https://github.com/CukierDev"),
+        ("Incognito-Coder", "https://github.com/Incognito-Coder"),
+        ("speedyfriend433", "https://github.com/speedyfriend433"),
+        ("Neo1102", "https://github.com/Neo1102"),
+        ("schweppes-0x", "https://github.com/schweppes-0x"),
+        ("LivioZ", "https://github.com/LivioZ"),
+        ("lockieluke", "https://github.com/lockieluke")
+    ]
     
     var body: some View {
         List {
-    
-            Section(header: Text("about_main_title".localized)) {
-                VStack(alignment: .leading) {
-                    HStack {
-                        Link("whoeevee", destination: URL(string: "https://github.com/whoeevee")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("asdfzxcvbn", destination: URL(string: "https://github.com/asdfzxcvbn")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
+            Section(header: Text("about_main_title".localized), alignment: .leading) {
+                ForEach(firstSectionLinks, id: \.0) { link in
+                    createLink(title: link.0, url: link.1)
                 }
             }
             
-            Section(header: Text("about_sec_title".localized), footer: Text("sort_source".localized)) {
-                VStack(alignment: .leading) {
-                    HStack {
-                        Link("ElliotCHEN37", destination: URL(string: "https://github.com/ElliotCHEN37")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("Richard-NDC", destination: URL(string: "https://github.com/Richard-NDC")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("5jd", destination: URL(string: "https://github.com/5jd")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("UnexcitingDean", destination: URL(string: "https://github.com/UnexcitingDean")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("gototheskinny", destination: URL(string: "https://github.com/gototheskinny")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("LIKVIDATOR1337", destination: URL(string: "https://github.com/LIKVIDATOR1337")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("xiangfeidexiaohuo", destination: URL(string: "https://github.com/xiangfeidexiaohuo")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("wlxxd", destination: URL(string: "https://github.com/wlxxd")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("longopy", destination: URL(string: "https://github.com/longopy")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("yodaluca23", destination: URL(string: "https://github.com/yodaluca23")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("3xynos7", destination: URL(string: "https://github.com/3xynos7")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("An0n-00", destination: URL(string: "https://github.com/An0n-00")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("by3lish", destination: URL(string: "https://github.com/by3lish")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("emal0n", destination: URL(string: "https://github.com/emal0n")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("CukierDev", destination: URL(string: "https://github.com/CukierDev")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("Incognito-Coder", destination: URL(string: "https://github.com/Incognito-Coder")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("speedyfriend433", destination: URL(string: "https://github.com/speedyfriend433")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("Neo1102", destination: URL(string: "https://github.com/Neo1102")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("schweppes-0x", destination: URL(string: "https://github.com/schweppes-0x")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("LivioZ", destination: URL(string: "https://github.com/LivioZ")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
-                    HStack {
-                        Link("lockieluke", destination: URL(string: "https://github.com/lockieluke")!)
-                        Spacer()
-                        Image(systemName: "arrow.up.right.square")
-                    }
+            Section(header: Text("about_sec_title".localized), footer: Text("sort_source".localized), alignment: .leading) {
+                ForEach(secondSectionLinks, id: \.0) { link in
+                    createLink(title: link.0, url: link.1)
                 }
             }
+        }
+    }
+    
+    private func createLink(title: String, url: String) -> some View {
+        HStack {
+            Link(title, destination: URL(string: url)!)
+            Spacer()
+            Image(systemName: "arrow.up.right.square")
         }
     }
 }
