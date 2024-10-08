dbab07e1c748cc598e399a4ab7e5432923f9b969
Elliot <elliotchen2010@gmail.com>
2024-10-08 19:25:38 +0800

    Update EeveeAboutSettingsView.swift

--- Sources/EeveeSpotify/Settings/Views/Sections/EeveeAboutSettingsView.swift
+++ Sources/EeveeSpotify/Settings/Views/Sections/EeveeAboutSettingsView.swift
@@ -1,42 +1,157 @@
 import SwiftUI
+import UIKit
 
 struct EeveeAboutSettingsView: View {
-    
-    let mainLinks = [
-        ("whoeevee", "https://github.com/whoeevee"),
-        ("asdfzxcvbn", "https://github.com/asdfzxcvbn")
-    ]
-    
-    let contributors = [
-        "ElliotCHEN37", "Richard-NDC", "5jd", "UnexcitingDean", "gototheskinny",
-        "LIKVIDATOR1337", "xiangfeidexiaohuo", "wlxxd", "longopy", "yodaluca23",
-        "3xynos7", "An0n-00", "by3lish", "emal0n", "CukierDev", "Incognito-Coder",
-        "speedyfriend433", "Neo1102", "schweppes-0x", "LivioZ", "lockieluke"
-    ]
-    
+
+	@State var latestVersion = ""
+
     var body: some View {
         List {
+		
+			VersionSection()
+			
             Section(header: Text("about_main_title".localized)) {
                 VStack(alignment: .leading) {
-                    ForEach(mainLinks, id: \.0) { name, url in
-                        Link(name, destination: URL(string: url)!)
+                    HStack {
+                        Link("whoeevee", destination: URL(string: "https://github.com/whoeevee")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("asdfzxcvbn", destination: URL(string: "https://github.com/asdfzxcvbn")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
                     }
                 }
             }
-            
-            Section(header: Text("about_sec_title".localized)) {
+
+            Section(header: Text("about_sec_title".localized), footer: Text("sort_source".localized)) {
                 VStack(alignment: .leading) {
-                    ForEach(contributors, id: \.self) { name in
-                        Link(name, destination: URL(string: "https://github.com/whoeevee/EeveeSpotify/graphs/contributors")!)
+                    HStack {
+                        Link("ElliotCHEN37", destination: URL(string: "https://github.com/ElliotCHEN37")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
                     }
-                }
-            }
-            
-            Section {
-                VStack(alignment: .leading) {
-                    Text("sort_source".localized)
+                    HStack {
+                        Link("Richard-NDC", destination: URL(string: "https://github.com/Richard-NDC")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("5jd", destination: URL(string: "https://github.com/5jd")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("UnexcitingDean", destination: URL(string: "https://github.com/UnexcitingDean")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("gototheskinny", destination: URL(string: "https://github.com/gototheskinny")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("LIKVIDATOR1337", destination: URL(string: "https://github.com/LIKVIDATOR1337")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("xiangfeidexiaohuo", destination: URL(string: "https://github.com/xiangfeidexiaohuo")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("wlxxd", destination: URL(string: "https://github.com/wlxxd")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("longopy", destination: URL(string: "https://github.com/longopy")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("yodaluca23", destination: URL(string: "https://github.com/yodaluca23")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("3xynos7", destination: URL(string: "https://github.com/3xynos7")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("An0n-00", destination: URL(string: "https://github.com/An0n-00")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("by3lish", destination: URL(string: "https://github.com/by3lish")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("emal0n", destination: URL(string: "https://github.com/emal0n")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("CukierDev", destination: URL(string: "https://github.com/CukierDev")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("Incognito-Coder", destination: URL(string: "https://github.com/Incognito-Coder")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("speedyfriend433", destination: URL(string: "https://github.com/speedyfriend433")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("Neo1102", destination: URL(string: "https://github.com/Neo1102")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("schweppes-0x", destination: URL(string: "https://github.com/schweppes-0x")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("LivioZ", destination: URL(string: "https://github.com/LivioZ")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
+                    HStack {
+                        Link("lockieluke", destination: URL(string: "https://github.com/lockieluke")!)
+                        Spacer()
+                        Image(systemName: "arrow.up.right.square")
+                    }
                 }
             }
         }
+		
+		.animation(.default, value: latestVersion)
+        .onAppear {
+            WindowHelper.shared.overrideUserInterfaceStyle(.dark)
+            
+            Task {
+                try await loadVersion()
+            }
+        }
+    }
+
+    func loadVersion() async throws {
+        let (data, _) = try await URLSession.shared.data(
+            from: URL(string: "https://api.github.com/repos/whoeevee/EeveeSpotify/releases/latest")!
+        )
+        
+        let tag = try JSONDecoder().decode(GitHubReleaseInfo.self, from: data).tag_name
+        latestVersion = String(tag.dropFirst(5))
     }
 }
